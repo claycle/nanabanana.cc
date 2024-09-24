93f21dc1c13f33a7025924c3a07bf9f01dd15d2f
C L <claycle@claycle.com>
2024-09-24 13:01:28 -0500

    IC.

---
+++ clean.sh
@@ -0,0 +1,20 @@
+#! /bin/zsh
+# A simple script to either run the trash command line (macos, added via Macports or Brew)
+# or just rely on rm -rf
+
+
+DIR="./public"
+CMD=""
+if [ -d ${DIR} ]; then
+    eval "which -s trash"
+    if [ $? -eq 0 ]; then
+        CMD="trash"
+    else
+        CMD="rm -rf"
+    fi
+    echo ${CMD} "${DIR}/*"
+    eval ${CMD} "${DIR}/*"
+else
+    echo "No $DIR"
+fi
+exit $?
